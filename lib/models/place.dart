6ce78a5a6c0a4c78b61045a2b89cacd7f709c61a
Atul Vinod <atul.v@keka.com>
2021-09-17 11:22:57 +0530

    created great places provider

---
+++ lib/models/place.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+
+class Place {
+  final String id;
+  final String title;
+  final PlaceLocation location;
+  final File image;
+  Place(
+      {required this.id,
+      required this.title,
+      required this.location,
+      required this.image});
+}
+
+class PlaceLocation {
+  final double lat;
+  final double long;
+  final String address;
+
+  PlaceLocation(this.lat, this.long, this.address);
+}
